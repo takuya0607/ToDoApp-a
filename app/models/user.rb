b8613cc5d7e9b9c29ea53a18ee5450256428acf7
takuya0607 <60646178+takuya0607@users.noreply.github.com>
2020-03-31 15:02:04 +0900

    commit_update

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :todos, dependent: :destroy
   has_one :userinfo, dependent: :destroy
+
+  validates :password, :email, presence: true
 end
--- db/migrate/20200228111035_create_todos.rb
+++ db/migrate/20200228111035_create_todos.rb
@@ -3,7 +3,7 @@ class CreateTodos < ActiveRecord::Migration[5.2]
     create_table :todos do |t|
       t.string :Title, null: false
       t.text :Detail, null: false
-
+      t.integer :user_id, null: false,foreign_key: true
       t.timestamps
     end
   end
