ace0aa6da40d26bfec9bcceb0e5ea8830bf71b86
takuya0607 <60646178+takuya0607@users.noreply.github.com>
2020-04-02 14:39:51 +0900

    commit_to_time

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module ToDoApp1
     # Initialize configuration defaults for originally generated Rails version.
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'ja.yml').to_s]
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
